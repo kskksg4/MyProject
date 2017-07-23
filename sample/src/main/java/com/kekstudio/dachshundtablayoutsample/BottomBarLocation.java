0e5dcefe47855161ba81a653f9fe1e3be63056a7
Sung-geun <kskksg4@naver.com>
2017-07-23 16:32:32 +0900

    bottombar add  bottombarLocation xml java

---
+++ sample/src/main/java/com/kekstudio/dachshundtablayoutsample/BottomBarLocation.java
@@ -0,0 +1,17 @@
+package com.kekstudio.dachshundtablayoutsample;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 성근 on 2017-06-29.
+ */
+
+public class BottomBarLocation extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bottombar_location);
+    }
+}
