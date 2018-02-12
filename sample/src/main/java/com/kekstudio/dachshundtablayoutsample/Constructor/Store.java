a232a075bd4345fc9faa0c6677a27e93f6176573
kskksg4 <kskksg4@gmail.com>
2018-02-13 06:21:18 +0900

    Add HttpTask

---
+++ sample/src/main/java/com/kekstudio/dachshundtablayoutsample/Constructor/Store.java
@@ -0,0 +1,44 @@
+package com.kekstudio.dachshundtablayoutsample.Constructor;
+
+/**
+ * Created by gimseong-geun on 2018. 2. 13..
+ */
+
+public class Store {
+    private String storeName;
+    private String address;
+    private String lat;
+    private String lng;
+
+    public String getStoreName() {
+        return storeName;
+    }
+
+    public void setStoreName(String storeName) {
+        this.storeName = storeName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLng() {
+        return lng;
+    }
+
+    public void setLng(String lng) {
+        this.lng = lng;
+    }
+}
--- sample/src/main/java/com/kekstudio/dachshundtablayoutsample/Fragment01.java
+++ sample/src/main/java/com/kekstudio/dachshundtablayoutsample/Fragment01.java
@@ -2,6 +2,7 @@ package com.kekstudio.dachshundtablayoutsample;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -15,10 +16,29 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.kekstudio.dachshundtablayoutsample.Constructor.Store;
 import com.poliveira.parallaxrecycleradapter.ParallaxRecyclerAdapter;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Vector;
 
 import static android.app.Activity.RESULT_OK;
 
@@ -29,6 +49,10 @@ import static android.app.Activity.RESULT_OK;
 public class Fragment01 extends Fragment {
 
     private static final int LOCATION_INTENT = 100;
+    private final String URLPATH = "http://116.255.77.198/firstapp/index.php";
+
+    private double lat;
+    private double lng;
 
     Context mContext;
 
@@ -126,13 +150,134 @@ public class Fragment01 extends Fragment {
         if(requestCode == LOCATION_INTENT){
             if(resultCode == RESULT_OK){
                 if(data.hasExtra("lat") && data.hasExtra("lng")){
-                    Double lat = data.getDoubleExtra("lat", 0);
-                    Double lng = data.getDoubleExtra("lng", 0);
+                    lat = data.getDoubleExtra("lat", 0);
+                    lng = data.getDoubleExtra("lng", 0);
                     Log.d("result", LOCATION_INTENT+"");
 
                     Toast.makeText(getContext(), lat+"와 "+lng, Toast.LENGTH_SHORT).show();
+                    new HttpTask().execute();
                 }
             }
+        }
+    }
+
+    private class HttpTask extends AsyncTask<Void, Void, ArrayList<Store>>{
+
+        @Override
+        protected ArrayList<Store> doInBackground(Void... voids) {
+
+            HttpPost request = new HttpPost(URLPATH);
+
+            ArrayList<Store> arrayList = new ArrayList<>();
+
+            Vector<NameValuePair> nameValue = new Vector<>();
+
+            try {
+                XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+                XmlPullParser xmlPullParser = factory.newPullParser();
+
+                nameValue.add(new BasicNameValuePair("lat", Double.toString(lat)));
+                nameValue.add(new BasicNameValuePair("lng", Double.toString(lng)));
+
+                HttpEntity enty = new UrlEncodedFormEntity(nameValue, HTTP.UTF_8);
+
+                request.setEntity(enty);
+
+                HttpClient client = new DefaultHttpClient();
+                HttpResponse res = client.execute(request);
+
+                HttpEntity entityResponse = res.getEntity();
+
+                InputStream im = entityResponse.getContent();
+
+                xmlPullParser.setInput(im, "utf-8");
+
+                Store store = null;
+
+                int eventType = xmlPullParser.getEventType();
+
+                while(eventType != XmlPullParser.END_DOCUMENT){
+                    switch(eventType){
+                        case XmlPullParser.START_DOCUMENT:
+                            Log.d("XML", "xmlStart");
+                            break;
+                        case XmlPullParser.START_TAG:
+                            Log.d("XML", "StartTag : "+xmlPullParser.getName());
+
+                            try{
+                                String startTag = xmlPullParser.getName();
+
+                                if(startTag.equals("mark")){
+                                    store = new Store();
+                                }
+
+                                if(startTag.equals("name")){
+                                    store.setStoreName(xmlPullParser.nextText());
+                                    Log.d("XML", "id"+xmlPullParser.getName());
+                                    Log.d("XML", "id"+xmlPullParser.getName());
+                                    Log.d("store", store.getStoreName());
+                                }
+
+                                if(startTag.equals("address")){
+                                    store.setAddress(xmlPullParser.nextText());
+                                    Log.d("XML", "id"+xmlPullParser.getName());
+                                    Log.d("XML", "id"+xmlPullParser.getName());
+                                }
+
+                                if(startTag.equals("lat")){
+                                    store.setLat(xmlPullParser.nextText());
+                                    Log.d("XML", "id"+xmlPullParser.getName());
+                                    Log.d("XML", "id"+xmlPullParser.getName());
+                                }
+
+                                if(startTag.equals("lng")){
+                                    store.setLng(xmlPullParser.nextText());
+                                    Log.d("XML", "id"+xmlPullParser.getName());
+                                    Log.d("XML", "id"+xmlPullParser.getName());
+                                }
+                            }catch(Exception e){
+                                e.printStackTrace();
+                            }
+                            break;
+                        case XmlPullParser.END_TAG:
+                            Log.d("XML", "EndTag"+xmlPullParser.getName());
+
+                            String endTag = xmlPullParser.getName();
+
+                            if(endTag.equals("mark")){
+                                arrayList.add(store);
+                            }
+                            break;
+                    }
+
+                    try{
+                        eventType = xmlPullParser.next();
+                    }catch(IOException e){
+                        e.printStackTrace();
+                    }
+                }
+
+                im.close();
+
+                return  arrayList;
+            } catch (XmlPullParserException e) {
+                e.printStackTrace();
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            } catch (ClientProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<Store> stores) {
+            super.onPostExecute(stores);
+
+            Log.d("stores", stores+"");
         }
     }
 
